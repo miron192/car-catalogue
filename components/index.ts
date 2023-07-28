35105c4e0fb289c567ecd89691a1949909015fb2
Miron Catalin <catalinmiron093@gmail.com>
2023-07-29 02:49:16 +0300

    init commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import SearchBar from "./Searchbar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CustomFilter from "./CustomFilter";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+  CustomButton,
+  Footer,
+  CarDetails,
+  Navbar,
+  CustomFilter,
+  SearchBar,
+  Hero,
+  SearchManufacturer,
+  ShowMore,
+  CarCard,
+};
